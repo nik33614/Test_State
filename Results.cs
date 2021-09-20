bb5292d013823fc8e0630db1ecb15fee30b4163f
Nikita <58114134+nik33614@users.noreply.github.com>
2021-09-20 17:02:14 +0300

    Add files via upload

---
+++ Results.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Results : MonoBehaviour
+{
+    /// <summary>
+    /// This script send information on server about this fight
+    /// 1. Send opponent's place.
+    /// 2. Send player's place
+    /// 3. Turn on opponent's cooldown
+    /// 4. Turn on player's cooldown
+    /// 5. Increase winner's level
+    /// 6. Give to winner 10% of opponent's money + reward
+    /// 
+    /// </summary>
+
+    int opponent_result;
+
+    void Start()
+    {
+        
+    }
+
+    private IEnumerator Get_Level()//remake
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("id", PlayerPrefs.GetInt("opponent_id").ToString());
+
+
+        WWW www = new WWW("http://doublenikmak.ru/Test_State/System/Get_Level.php", form);
+
+        yield return www;
+        if (www.error != null)
+        {
+            Debug.Log("Произошла ошибка: " + www.error);
+            yield break;
+        }
+
+        else
+        {
+            PlayerPrefs.SetInt("level", Convert.ToInt32(www.text));
+            StartCoroutine(Get_Damage());
+        }
+    }
+
+}
